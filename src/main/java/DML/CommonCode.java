f3f9142295c6d3d5dd917bdc49e40cf5a6292b84
Ali Shan Khawaja <al290543@dal.ca>
2025-06-16 15:01:25 -0400

    DML Module

---
+++ src/main/java/DML/CommonCode.java
@@ -0,0 +1,37 @@
+package DML;
+
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class CommonCode {
+
+    public boolean checkIfFileExists(String filePath) {
+        File f = new File(filePath);
+        return f.exists();
+    }
+
+    public void writeBufferToFile(String filePath, ArrayList<String> buffer) {
+
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(filePath);
+            writer.print("");
+            writer.flush();
+
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
+            int index = 0;
+            for (String value : buffer) {
+                bufferedWriter.write(value);
+                if (index < buffer.size() - 1) {
+                    bufferedWriter.newLine();
+                }
+                index += 1;
+            }
+
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
